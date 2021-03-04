6294c862a0dbf682c22454f5f0f3fcd9c5f6681f
JvSe <joaovitorsoares12@gmail.com>
2021-03-04 09:49:39 -0300

    Iniciando o projeto

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/jvse.png" alt="Joao Vitor Soares" />
+            <div>
+                <strong> Joao Vitor Soares</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
